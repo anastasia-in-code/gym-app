b266bf38e329727d9e0a7294aeebba677686b06e
Anastasiia <anastasiia.perih@gmail.com>
2023-11-28 13:25:52 -0500

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Box, Stack, Typography } from '@mui/material'
+
+import fitLogo from '../assets/imgs/logo.png'
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Box mt='80px' bgcolor='#fff3f4'>
+      <Stack gap='40px' alignItems='center' px='40px' pt='24px'>
+        <Stack direction='row' >
+          <img src={fitLogo} alt='logo' height='40px' />
+          <Typography color='primeRed' fontWeight='600px' fontSize='26px'>
+            GymFlex Pro
+          </Typography>
+        </Stack>
+        <Typography variant='h6' pb='40px' >Created with ❤️ by Anastasiia Perih </Typography>
+      </Stack>
+
+    </Box>
   )
 }
 
