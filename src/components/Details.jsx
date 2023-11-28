f2c1d28734270b4d3ba94e289395cb3dccb33c37
Anastasiia <anastasiia.perih@gmail.com>
2023-11-27 22:16:47 -0500

    exercise details

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,8 +1,51 @@
 import React from 'react'
+import { Typography, Stack, Button } from '@mui/material'
 
-const Details = () => {
+import equipmentIcon from '../assets/icons/equipment.png'
+import targetIcon from '../assets/icons/target.png'
+import bodyPartIcon from '../assets/icons/bodyPart.png'
+
+const Details = ({ exercisesDetailsData }) => {
+  const { bodyPart, gifUrl, name, target, equipment } = exercisesDetailsData
+
+  const extraDetails = [
+    {
+      icon: bodyPartIcon,
+      name: bodyPart
+    }, {
+      icon: targetIcon,
+      name: target
+    }, {
+      icon: equipmentIcon,
+      name: equipment
+    }
+  ]
+
   return (
-    <div>Details</div>
+    <Stack gap='60px' sx={{ flexDirection: { lg: 'row' }, p: '20px', alignItems: 'center' }} >
+      <img src={gifUrl} alt={name} loading='lazy' className='details-image' />
+      <Stack sx={{ gap: { lg: '35px', xs: '20px' } }}>
+        <Typography variant='h3' sx={{ textTransform: 'capitalize' }}>
+          {name}
+        </Typography>
+        <Typography variant='h6'>
+          Exercises keep you strong.{' '}
+          <span style={{ textTransform: 'capitalize' }}>{name}</span> is one
+          of the best <br /> exercises to target your {target}. It will help you improve your{' '}
+          <br /> mood and gain energy.
+        </Typography>
+        {extraDetails.map(item => (
+          <Stack key={item.name} direction='row' gap='24px' alignItems='center'>
+            <Button sx={{backgroundColor: '#fff2db', borderRadius: '50%', width: '100px', height: '100px' }}>
+              <img src={item.icon} alt={bodyPart} style={{width:'50px', height: '50px'}}/>
+            </Button>
+            <Typography textTransform='capitalize' variant='h5'>
+              {item.name}
+            </Typography>
+          </Stack>
+        ))}
+      </Stack>
+    </Stack>
   )
 }
 
--- src/pages/ExerciseDetails.jsx
+++ src/pages/ExerciseDetails.jsx
@@ -1,15 +1,35 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { Box } from '@mui/material'
 
-import {exercisesRequestOptions, fetchExercises} from '../utils/fetchExercises'
+import { exercisesRequestOptions, fetchExercises } from '../utils/fetchExercises'
 import Details from '../components/Details'
 import Videos from '../components/Videos'
 import Similar from '../components/Similar'
 
 const ExersizeDetails = () => {
+  const [exersiceDetails, setExersiceDetails] = useState({})
+  const {id} = useParams()
+
+  useEffect(() => {
+    const fetchExerciseData = async () => {
+      const exersicedbUrl = 'https://exercisedb.p.rapidapi.com/exercises'
+      const youtubedbUrl = 'https://youtube-search-and-download.p.rapidapi.com'
+
+      const exercizeDetailsData = await fetchExercises(`${exersicedbUrl}/exercise/${id}`, exercisesRequestOptions)
+      console.log(exercizeDetailsData)
+      setExersiceDetails(exercizeDetailsData)
+    }
+
+    fetchExerciseData()
+  }, [id])
   return (
-    <div>ExersizeDetails</div>
+    <Box>
+      <Details exercisesDetailsData={exersiceDetails}/>
+      <Videos />
+      <Similar />
+    </Box>
+
   )
 }
 
